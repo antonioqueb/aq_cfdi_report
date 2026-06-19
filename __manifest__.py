05058cd3d48afc89acff0aa671c3d22db261e665
Antonio Queb <cardstillo@gmail.com>
2026-06-19 03:01:45 -0600

    Agregar plantilla de reporte de orden de venta y actualizar dependencias en el manifiesto para incluirla.

--- __manifest__.py
+++ __manifest__.py
@@ -6,10 +6,11 @@
     "author": "Alphaqueb Consulting SAS",
     "license": "LGPL-3",
     "category": "Accounting/Localizations",
-    "depends": ["account", "l10n_mx_edi"],
+    "depends": ["account", "l10n_mx_edi", "sale"],
     "data": [
         "report/report_disable_l10n_mx.xml",
         "report/report_cfdi_template.xml",
+        "report/report_saleorder_template.xml",
     ],
     "external_dependencies": {"python": ["qrcode"]},
     "installable": True,
